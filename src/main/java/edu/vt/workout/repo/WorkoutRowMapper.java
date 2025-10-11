15e57c472d8f8ba83c8a509f72424687ad9d6071
oliviasolomon <oliviasolomon@vt.edu>
2025-10-11 15:15:14 -0400

    Create WorkoutRowMapper.java

---
+++ src/main/java/edu/vt/workout/repo/WorkoutRowMapper.java
@@ -0,0 +1,25 @@
+package edu.vt.workout.repo;
+
+import edu.vt.workout.model.Workout;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class WorkoutRowMapper implements RowMapper<Workout> {
+  @Override
+  public Workout mapRow(ResultSet rs, int rowNum) throws SQLException {
+    Workout w = new Workout();
+    w.setId(rs.getLong("id"));
+    w.setUserId(rs.getInt("user_id"));
+    w.setExerciseName(rs.getString("exercise_name"));
+    w.setMuscleGroup(rs.getString("muscle_group"));
+    w.setSets(rs.getInt("sets"));
+    w.setReps(rs.getInt("reps"));
+    double weight = rs.getDouble("weight");
+    if (!rs.wasNull()) w.setWeight(weight);
+    w.setDate(rs.getTimestamp("date") != null ? rs.getTimestamp("date").toLocalDateTime() : LocalDateTime.now());
+    return w;
+  }
+}
