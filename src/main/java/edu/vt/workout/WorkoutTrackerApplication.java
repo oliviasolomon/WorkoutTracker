18ea09d50e612540e48c9d51648dd2aa35b35b29
oliviasolomon <oliviasolomon@vt.edu>
2025-10-11 18:10:19 +0000

    Upload New File

---
+++ src/main/java/edu/vt/workout/WorkoutTrackerApplication.java
@@ -0,0 +1,11 @@
+package edu.vt.workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WorkoutTrackerApplication.class, args);
+  }
+}
