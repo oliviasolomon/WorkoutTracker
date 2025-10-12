dcb146fd9a65c3d1a73b1c432b1e1dc003106fc4
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 12:39:46 -0400

    Update HomeController.java

--- src/main/java/edu/vt/workout/controller/HomeController.java
+++ src/main/java/edu/vt/workout/controller/HomeController.java
@@ -1,12 +1,9 @@
 package edu.vt.workout.controller;
 
-import edu.vt.workout.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
-// Serve static index.html from src/main/resources/static/index.html
+// Serve static pages
 @Controller
 public class HomeController {
 
@@ -15,30 +12,13 @@ public class HomeController {
         return "index.html";
     }
 
-    // Simple REST endpoints for signup/login; front-end can POST JSON to these
-    @RestController
-    @RequestMapping("/api/auth")
-    public static class AuthController {
-        private final UserService userService;
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup.html";
+    }
 
-        @Autowired
-        public AuthController(UserService userService) {
-            this.userService = userService;
-        }
-
-        record AuthRequest(String username, String password) {}
-
-        @PostMapping("/signup")
-        public ResponseEntity<String> signup(@RequestBody AuthRequest req) {
-            userService.register(req.username(), req.password());
-            return ResponseEntity.ok("ok");
-        }
-
-        @PostMapping("/login")
-        public ResponseEntity<String> login(@RequestBody AuthRequest req) {
-            boolean ok = userService.authenticate(req.username(), req.password());
-            if (ok) return ResponseEntity.ok("ok");
-            return ResponseEntity.status(401).body("invalid");
-        }
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login.html";
     }
 }
