5525e4b267f5411872f0bea848a81fafc1248fcf
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 11:32:47 -0400

    Update UserRepository.java

--- src/main/java/edu/vt/workout/repo/UserRepository.java
+++ src/main/java/edu/vt/workout/repo/UserRepository.java
@@ -1,8 +1,11 @@
 package edu.vt.workout.repo;
 
-import edu.vt.workout.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import edu.vt.workout.model.User;
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
