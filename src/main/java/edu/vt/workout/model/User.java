5cb0e92ae562b854af574c1597e12751017e9082
greergurd <158761233+greergurd@users.noreply.github.com>
2025-10-12 16:45:50 -0400

    Update User.java - adding getEmail()

--- src/main/java/edu/vt/workout/model/User.java
+++ src/main/java/edu/vt/workout/model/User.java
@@ -24,6 +24,9 @@ public class User {
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
     public String getPassword() { return passwordHash; }
     public void setPassword(String passwordHash) { this.passwordHash = passwordHash; }
 }
