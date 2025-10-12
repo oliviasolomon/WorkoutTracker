7dd627bedc9b0b8f0f4eb5a2bc584f926bd5ca4c
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 09:29:33 -0400

    Update User.java

--- src/main/java/edu/vt/workout/model/User.java
+++ src/main/java/edu/vt/workout/model/User.java
@@ -1,49 +1,27 @@
 package edu.vt.workout.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    @Column(unique = true, nullable = false)
     private String username;
+    private String password;
 
-    @Column(name = "password_hash", nullable = false)
-    private String passwordHash;
+    // getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public User() {}
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public User(String username, String passwordHash) {
-        this.username = username;
-        this.passwordHash = passwordHash;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
