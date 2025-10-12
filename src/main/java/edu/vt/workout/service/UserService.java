e5d811af9dd20bab0f421aa49164b833bba3055d
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 02:11:39 -0400

    Update UserService.java

--- src/main/java/edu/vt/workout/service/UserService.java
+++ src/main/java/edu/vt/workout/service/UserService.java
@@ -1,35 +1,22 @@
 package edu.vt.workout.service;
 
-import edu.vt.workout.model.User;
-import edu.vt.workout.repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public String encodePassword(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
     }
 
-    public User createUser(String username, String password) {
-        String hash = passwordEncoder.encode(password);
-        User user = new User(username, hash);
-        return userRepository.save(user);
-    }
-
-    public boolean checkPassword(User user, String password) {
-        return passwordEncoder.matches(password, user.getPasswordHash());
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
     }
 }
