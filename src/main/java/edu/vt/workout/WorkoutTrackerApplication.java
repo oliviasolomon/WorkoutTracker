a9e6072bc1bcf48540c9a79c3b8022328d5ff73a
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 02:03:16 -0400

    Update WorkoutTrackerApplication.java

--- src/main/java/edu/vt/workout/WorkoutTrackerApplication.java
+++ src/main/java/edu/vt/workout/WorkoutTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkoutTrackerApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(WorkoutTrackerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutTrackerApplication.class, args);
+    }
 }
