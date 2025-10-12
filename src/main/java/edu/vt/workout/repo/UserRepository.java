39a167fe76945df8c46d1d62b0d9c4c2a3eb0b78
oliviasolomon <oliviasolomon@vt.edu>
2025-10-12 01:59:58 -0400

    Create UserRepository.java

---
+++ src/main/java/edu/vt/workout/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package edu.vt.workout.repo;
+
+import com.example.workouttracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
