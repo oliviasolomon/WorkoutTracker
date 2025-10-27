899716900d53ef1b4d3fd10b0d4d5c9239858a1c
oliviasolomon <oliviasolomon@vt.edu>
2025-10-26 23:01:01 -0400

    adding comments

--- src/main/java/edu/vt/workout/controller/ExerciseController.java
+++ src/main/java/edu/vt/workout/controller/ExerciseController.java
@@ -8,19 +8,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/exercises")
-@CrossOrigin(origins = {"https://workouttracker-d5wa.onrender.com", "http://localhost:8080", "http://localhost:3000"})
+@CrossOrigin(origins = {"https://workouttracker-d5wa.onrender.com"})
 public class ExerciseController {
 
-    private final JdbcTemplate jdbc;
+    private final JdbcTemplate jdbc; // helper to directly execute sql queries
 
-    @Autowired
+    @Autowired // auto injects jbdctemplate from spring's application content
     public ExerciseController(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
 
-    @GetMapping
+    @GetMapping // handles getters
     public List<String> listExercises() {
+        // sql query to fetch all exercise names from exercises table in schema in alphabetical order
         String sql = "SELECT name FROM exercises ORDER BY name ASC";
+        // executes query and returns a list of exercise names as strings
         return jdbc.queryForList(sql, String.class);
     }
 }
