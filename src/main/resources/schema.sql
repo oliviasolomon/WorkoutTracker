f237cfc5fb13835ebfd982730088ddb48594c7ac
oliviasolomon <oliviasolomon@vt.edu>
2025-10-26 22:56:00 -0400

    adding comments

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,43 +1,43 @@
--- Users table
+-- table to store user accounts
 CREATE TABLE IF NOT EXISTS users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    id BIGINT AUTO_INCREMENT PRIMARY KEY, --unique identifier
+    username VARCHAR(50) NOT NULL UNIQUE, -- username
+    password VARCHAR(255) NOT NULL -- password
 );
 
--- Workouts table
+-- table to define workouts
 CREATE TABLE IF NOT EXISTS workouts (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    muscle_group VARCHAR(100),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    sets INT,
-    reps INT,
-    weight DECIMAL (6,2),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    id BIGINT AUTO_INCREMENT PRIMARY KEY, -- unique workout id
+    user_id BIGINT NOT NULL, -- links workout to user
+    name VARCHAR(100) NOT NULL, -- workout name
+    muscle_group VARCHAR(100), -- targeted muscle group
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- logged workout date
+    sets INT NOT NULL, -- number of sets
+    reps INT NOT NULL, -- number of reps
+    weight DECIMAL (6,2), -- weight used
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- deletes workout upon account deletion
 );
 
--- Exercises table
+-- table for exercises in a workout
 CREATE TABLE IF NOT EXISTS exercises (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    workout_id BIGINT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    sets INT NOT NULL,
-    reps INT NOT NULL,
-    weight DECIMAL(6,2),
-    FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
+    id BIGINT AUTO_INCREMENT PRIMARY KEY, -- unique exercise id
+    workout_id BIGINT NOT NULL, -- links exrecise to a workout
+    name VARCHAR(100) NOT NULL, -- exercise name
+    sets INT NOT NULL, -- number of sets
+    reps INT NOT NULL, -- number of reps
+    weight DECIMAL(6,2), -- weight used
+    FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE -- deletes exercises upon workout deletion
 );
 
--- Logs table
+-- table to store workout logs
 CREATE TABLE IF NOT EXISTS logs (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  workout_id BIGINT NOT NULL,
-  user_id BIGINT NOT NULL,
-  sets INT NOT NULL,
-  reps INT NOT NULL,
-  weight DECIMAL(6,2),
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  id BIGINT AUTO_INCREMENT PRIMARY KEY, -- unique log id
+  workout_id BIGINT NOT NULL, -- references workout performed
+  user_id BIGINT NOT NULL, -- references user who performed workout
+  sets INT NOT NULL, -- sets completed
+  reps INT NOT NULL, -- reps completed
+  weight DECIMAL(6,2), -- weight used
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- date/time of logged workout
+  FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE, -- deletes logs upon workout deletion
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- deletes logs upon user deletion
 );
