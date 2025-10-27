f54ca81ae871987a81d00021ea4ea83fbe93220d
oliviasolomon <oliviasolomon@vt.edu>
2025-10-26 23:04:54 -0400

    adding comments

--- src/main/java/edu/vt/workout/controller/HomeController.java
+++ src/main/java/edu/vt/workout/controller/HomeController.java
@@ -3,20 +3,25 @@ package edu.vt.workout.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// Serve static pages
+// ---
+// this controller handles routing for the frontend static html pages directly
+// --
 @Controller
 public class HomeController {
 
+    //maps the root url to the homepage
     @GetMapping("/")
     public String index() {
         return "index.html";
     }
 
-    @GetMapping("/signup")
+    //maps "/signup "to the signup page
+    @GetMapping("/signup") 
     public String signupPage() {
         return "signup.html";
     }
 
+    //maps "/login" to the login page
     @GetMapping("/login")
     public String loginPage() {
         return "login.html";
