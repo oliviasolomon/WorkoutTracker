cc764e4f70e359778de8126071783b511c299972
oliviasolomon <oliviasolomon@vt.edu>
2025-10-26 22:23:06 -0400

    trying to get exercise name to work

--- src/main/java/edu/vt/workout/repo/WorkoutRowMapper.java
+++ src/main/java/edu/vt/workout/repo/WorkoutRowMapper.java
@@ -10,14 +10,19 @@ public class WorkoutRowMapper implements RowMapper<Workout> {
   @Override
   public Workout mapRow(ResultSet rs, int rowNum) throws SQLException {
     Workout w = new Workout();
-    w.setId(rs.getLong("id"));
+    w.setId(rs.getObject("id", Long.class));
     w.setUserId(rs.getObject("user_id", Long.class));
+    // idk whats going on with exercise name
+    String exercise = rs.getString("exercise_name");
+    if (exercise == null) exercise = rs.getString("name");
+    w.setExerciseName(exercise);
+    
     w.setExerciseName(rs.getString("exercise_name"));
     w.setMuscleGroup(rs.getString("muscle_group"));
     w.setSets(rs.getInt("sets"));
     w.setReps(rs.getInt("reps"));
-     //nullable field
-    w.setWeight(rs.getObject("weight", Double.class));
+    w.setWeight(rs.getObject("weight", Double.class));  //nullable field
+    w.setUnits(rs.getString("units"));
     if (rs.getTimestamp("date") != null) w.setDate(rs.getTimestamp("date").toLocalDateTime());
     return w;
   }
