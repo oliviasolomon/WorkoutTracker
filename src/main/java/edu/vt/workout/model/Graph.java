588beb403b337799c38b87e1ccaa7cbde4a5d661
jbrent222024S <jbrent22@vt.edu>
2025-10-28 14:35:46 -0400

    Added graph class
    Added jfreechart for plotting

---
+++ src/main/java/edu/vt/workout/model/Graph.java
@@ -0,0 +1,15 @@
+package edu.vt.workout.model;
+
+// Drawn from an online example: https://codingtechroom.com/question/plot-graphs-in-java
+
+import org.jfree.chart.ChartFactory;
+
+
+public class Graph {
+    //~ Fields ................................................................
+    
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
