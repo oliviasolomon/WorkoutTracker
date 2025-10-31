8aaa583b418b183e843f3ad10c9a69cb0a77772b
oliviasolomon <oliviasolomon@vt.edu>
2025-10-30 21:22:12 -0400

    adding comments

--- src/main/java/edu/vt/workout/repo/UserRepository.java
+++ src/main/java/edu/vt/workout/repo/UserRepository.java
@@ -4,6 +4,14 @@ import edu.vt.workout.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+//--
+// extends the spring data jpa's jparepository to provide crud operations
+// for the user entity without requiring manual sql.
+//
+// used by: usercontroller, userservice
+//--
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    // finder method (like SELECT * FROM users WHERE username = ?)
     Optional<User> findByUsername(String username);
 }
