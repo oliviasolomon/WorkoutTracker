c4d1516c56ddf0e66b58f67710ff51651b9649cc
greergurd <158761233+greergurd@users.noreply.github.com>
2025-10-31 14:25:38 -0400

    Create Metrics.Controller.java

---
+++ src/main/java/edu/vt/workout/controller/Metrics.Controller.java
@@ -0,0 +1,54 @@
+package edu.vt.workout.controller;
+
+import edu.vt.workout.model.Graph;
+import edu.vt.workout.model.Log;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.TimeZone;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class MetricsController {
+
+    @GetMapping(value = "/metrics/chart", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> getMetricsChart() {
+        try {
+            // Generate dummy data (you can later replace this with DB logs)
+            Log[] logbook = generateSampleLogs(50);
+
+            // Use Graph to create a chart image in memory
+            Graph graph = new Graph(TimeZone.getDefault());
+            BufferedImage chartImage = graph.createChartImage(logbook); // new helper method below
+
+            // Convert BufferedImage -> byte[]
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(chartImage, "png", baos);
+            return ResponseEntity.ok(baos.toByteArray());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    private Log[] generateSampleLogs(int n) {
+        Random rand = new Random();
+        Log[] logs = new Log[n];
+        for (int i = 0; i < n; i++) {
+            Log l = new Log();
+            l.setDate(LocalDateTime.now().minusDays(rand.nextInt(30)));
+            l.setSets(rand.nextInt(5) + 3);
+            l.setReps(rand.nextInt(10) + 5);
+            l.setWeight(rand.nextInt(100) + 100);
+            logs[i] = l;
+        }
+        return logs;
+    }
+}
