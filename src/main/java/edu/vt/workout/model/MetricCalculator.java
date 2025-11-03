ae9b7d40faa5d8621afdc34e0ffa4fc19c9a27a7
jbrent222024S <jbrent22@vt.edu>
2025-11-03 11:59:10 -0500

    Added functionality to MetricCalculator
    now calculates lean body mass

--- src/main/java/edu/vt/workout/model/MetricCalculator.java
+++ src/main/java/edu/vt/workout/model/MetricCalculator.java
@@ -1,11 +1,19 @@
 package edu.vt.workout.model;
 
+// -------------------------------------------------------------------------
+/**
+ *  Class for calculating different body metrics
+ * 
+ *  @author jbrent22
+ *  @version Nov 3, 2025
+ */
 public class MetricCalculator
 {
     // ~Public Methods ........................................................
     // ----------------------------------------------------------
     /**
-     * Calculates BMI based off of height and weight
+     * Calculates BMI based off of height and weight using the formula found
+     * here: https://www.calculator.net/bmi-calculator.html
      * 
      * @param height
      *            in inches
@@ -24,7 +32,6 @@ public class MetricCalculator
      * Calculates fat percentage based on metrics listed below. Hip measurements
      * are only used when sex = "female". All units are in pounds or inches
      * 
-     * @param age
      * @param height
      * @param weight
      * @param sex
@@ -33,8 +40,7 @@ public class MetricCalculator
      * @param hips
      * @return double of percentage body fat (between 0 and 100)
      */
-    public double calcularteFatPercent(
-        int age,
+    public double calculateFatPercent(
         double height,
         double weight,
         String sex,
@@ -46,30 +52,53 @@ public class MetricCalculator
         // specified, averages the two equations
         if (sex.equalsIgnoreCase("male"))
         {
-            return fatPercentMale(age, height, weight, neck, waist);
+            return fatPercentMale(height, weight, neck, waist);
         }
         else if (sex.equalsIgnoreCase("female"))
         {
-            return fatPercentFemale(age, height, weight, neck, waist, hips);
+            return fatPercentFemale(height, weight, neck, waist, hips);
         }
         else
         {
-            double fpm = fatPercentMale(age, height, weight, neck, waist);
-            double fpf =
-                fatPercentFemale(age, height, weight, neck, waist, hips);
+            double fpm = fatPercentMale(height, weight, neck, waist);
+            double fpf = fatPercentFemale(height, weight, neck, waist, hips);
             return (fpm + fpf) / 2;
         }
     }
 
 
-    // Used the bfp formula for males in USC units found here:
-    // https://www.calculator.net/body-fat-calculator.html
-    private double fatPercentMale(
-        int age,
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * 
+     * @param height
+     * @param weight
+     * @param sex
+     * @param neck
+     * @param waist
+     * @param hips
+     * @return double pounds mass of everything except fat
+     */
+    public double leanBodyMass(
         double height,
         double weight,
+        String sex,
         double neck,
-        double waist)
+        double waist,
+        Double hips)
+    {
+        double percentLean =
+            1 - (calculateFatPercent(height, weight, sex, neck, waist, hips)
+                / 100);
+        return percentLean * weight;
+    }
+
+
+    // Used the bfp formula for males in USC units found here:
+    // https://www.calculator.net/body-fat-calculator.html
+    private
+        double
+        fatPercentMale(double height, double weight, double neck, double waist)
     {
         double term1 = 86.010 * Math.log10(waist - neck);
         double term2 = 70.041 * Math.log10(height);
@@ -80,7 +109,6 @@ public class MetricCalculator
     // Used the bfp formula for females in USC units found here:
     // https://www.calculator.net/body-fat-calculator.html
     private double fatPercentFemale(
-        int age,
         double height,
         double weight,
         double neck,
