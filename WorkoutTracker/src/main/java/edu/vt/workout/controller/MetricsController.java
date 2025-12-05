fb49bea2279a20a5cc2a0c3c0e02dd3fe24de467
oliviasolomon <oliviasolomon@vt.edu>
2025-12-05 12:31:54 -0500

    add real database data to metrics charts

--- WorkoutTracker/src/main/java/edu/vt/workout/controller/MetricsController.java
+++ WorkoutTracker/src/main/java/edu/vt/workout/controller/MetricsController.java
@@ -1,54 +1,59 @@
 package edu.vt.workout.controller;
 
-import edu.vt.workout.model.LogGraph;
 import edu.vt.workout.model.Log;
+import edu.vt.workout.repo.LogRowMapper;
+import edu.vt.workout.model.LogGraph;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.time.LocalDateTime;
-import java.util.Random;
+import java.util.List;
 import java.util.TimeZone;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
 @RestController
 public class MetricsController {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final LogRowMapper logRowMapper = new LogRowMapper();
+
+    public MetricsController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @GetMapping(value = "/metrics/chart", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> getMetricsChart() {
         try {
-            // Generate dummy data (you can later replace this with DB logs)
-            Log[] logbook = generateSampleLogs(50);
-
-            // Use Graph to create a chart image in memory
+            Log[] logs = fetchLogsFromDb();
+            // use graph to create chart image in memory
             LogGraph graph = new LogGraph(TimeZone.getDefault());
-            BufferedImage chartImage = graph.createChartImage(logbook); // new helper method below
-
-            // Convert BufferedImage -> byte[]
+            BufferedImage chartImage = graph.createChartImage(logs);
+            // convert buffered image > byte[]
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(chartImage, "png", baos);
-            return ResponseEntity.ok(baos.toByteArray());
 
+            return ResponseEntity.ok(baos.toByteArray());
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
         }
     }
 
-    private Log[] generateSampleLogs(int n) {
-        Random rand = new Random();
-        Log[] logs = new Log[n];
-        for (int i = 0; i < n; i++) {
-            Log l = new Log();
-            l.setDate(LocalDateTime.now().minusDays(rand.nextInt(30)));
-            l.setSets(rand.nextInt(5) + 3);
-            l.setReps(rand.nextInt(10) + 5);
-            l.setWeight(Double.valueOf(rand.nextInt(100) + 100)); //double expected for weight 
-            logs[i] = l;
-        }
-        return logs;
+    private Log[] fetchLogsFromDb() {
+        String sql = """
+            SELECT id, workout_id, user_id, sets, reps, weight, date
+            FROM logs
+            ORDER BY date DESC
+            LIMIT 50
+        """;
+
+        List<Log> list = jdbcTemplate.query(sql, logRowMapper);
+        return list.toArray(new Log[0]);
     }
 }
